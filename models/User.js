23a7f76f09f9713c03016c3c861de96d1b61216e
abmetheny <abmetheny@gmail.com>
2023-04-27 22:22:22 -0400

    Populated models files with model schemas

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, Types } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+      },
+      thoughts: {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+      friends: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      }
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = userSchema;
